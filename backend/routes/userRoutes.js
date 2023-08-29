d5dd506ece0cbf30a92c3fee2c3cd1d9f2c83e0e
Justin Shaffer <justinbshaffer91@gmail.com>
2023-08-29 16:46:09 -0700

    Enhance userRoutes.js
    - Input validation for the registration route
    - JWT authentication middleware (authenticateJWT) to protect routes that require user authentication
    - JWT middleware is applied to the profile fetch, update, and delete routes to ensure that only authenticated users can perform these actions
    - Passwords are removed from the user object before sending it in the profile fetch route for better security practices

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { ObjectId } = require('mongodb');
+const { check, validationResult } = require('express-validator');
 
 const USER_EXISTS_ERROR = 'User already exists.';
 const USER_NOT_FOUND_ERROR = 'User not found!';
@@ -13,109 +14,135 @@ const PROFILE_DELETE_FAILED_ERROR = 'Profile deletion failed.';
 // Utility function to get users collection
 const getUsersCollection = (req) => req.app.locals.db.collection('users');
 
-// User Registration
-router.post('/register', async (req, res, next) => {
-  try {
-    const usersCollection = getUsersCollection(req);
-    const { username, email, password } = req.body;
+// Middleware for JWT authentication
+const authenticateJWT = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
 
-    const existingUser = await usersCollection.findOne({ email });
-    if (existingUser) {
-      return res.status(400).send(USER_EXISTS_ERROR);
+// User Registration with input validation
+router.post('/register', [
+    check('email').isEmail(),
+    check('password').isLength({ min: 6 }),
+    check('username').notEmpty()
+], async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await usersCollection.insertOne({ username, email, password: hashedPassword });
-    res.status(201).send('User registered successfully!');
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const usersCollection = getUsersCollection(req);
+        const { username, email, password } = req.body;
+
+        const existingUser = await usersCollection.findOne({ email });
+        if (existingUser) {
+            return res.status(400).send(USER_EXISTS_ERROR);
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await usersCollection.insertOne({ username, email, password: hashedPassword });
+        res.status(201).send('User registered successfully!');
+    } catch (error) {
+        next(error);
+    }
 });
 
 // User Login
 router.post('/login', async (req, res, next) => {
-  try {
-    const usersCollection = getUsersCollection(req);
-    const { email, password } = req.body;
+    try {
+        const usersCollection = getUsersCollection(req);
+        const { email, password } = req.body;
 
-    const user = await usersCollection.findOne({ email });
+        const user = await usersCollection.findOne({ email });
 
-    if (!user) {
-      return res.status(400).send(USER_NOT_FOUND_ERROR);
+        if (!user) {
+            return res.status(400).send(USER_NOT_FOUND_ERROR);
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).send(INVALID_CREDENTIALS_ERROR);
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ token });
+    } catch (error) {
+        next(error);
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(400).send(INVALID_CREDENTIALS_ERROR);
-    }
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({ token });
-  } catch (error) {
-    next(error);
-  }
 });
 
-// Fetch User Profile
-router.get('/profile/:userId', async (req, res, next) => {
-  try {
-    const usersCollection = getUsersCollection(req);
-    const userId = req.params.userId;
+// Fetch User Profile with authentication middleware
+router.get('/profile/:userId', authenticateJWT, async (req, res, next) => {
+    try {
+        const usersCollection = getUsersCollection(req);
+        const userId = req.params.userId;
 
-    const user = await usersCollection.findOne({ _id: ObjectId(userId) });
+        const user = await usersCollection.findOne({ _id: ObjectId(userId) });
 
-    if (!user) {
-      return res.status(404).send(USER_NOT_FOUND_ERROR);
+        if (!user) {
+            return res.status(404).send(USER_NOT_FOUND_ERROR);
+        }
+
+        delete user.password; // Remove password before sending
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
     }
-
-    res.status(200).json(user);
-  } catch (error) {
-    next(error);
-  }
 });
 
 // Update User Profile
-router.put('/profile/:userId', async (req, res, next) => {
-  try {
-    const usersCollection = getUsersCollection(req);
-    const userId = req.params.userId;
-    const { username, email, age, height, weight } = req.body;
+router.put('/profile/:userId', authenticateJWT, async (req, res, next) => {
+    try {
+        const usersCollection = getUsersCollection(req);
+        const userId = req.params.userId;
+        const { username, email, age, height, weight } = req.body;
 
-    const result = await usersCollection.updateOne({ _id: ObjectId(userId) }, { $set: { username, email, age, height, weight } });
+        const result = await usersCollection.updateOne({ _id: ObjectId(userId) }, { $set: { username, email, age, height, weight } });
 
-    if (result.modifiedCount === 0) {
-      return res.status(400).send(PROFILE_UPDATE_FAILED_ERROR);
+        if (result.modifiedCount === 0) {
+            return res.status(400).send(PROFILE_UPDATE_FAILED_ERROR);
+        }
+
+        res.status(200).send('Profile updated successfully!');
+    } catch (error) {
+        next(error);
     }
-
-    res.status(200).send('Profile updated successfully!');
-  } catch (error) {
-    next(error);
-  }
 });
 
 // Delete User Profile
-router.delete('/profile/:userId', async (req, res, next) => {
-  try {
-    const usersCollection = getUsersCollection(req);
-    const userId = req.params.userId;
+router.delete('/profile/:userId', authenticateJWT, async (req, res, next) => {
+    try {
+        const usersCollection = getUsersCollection(req);
+        const userId = req.params.userId;
 
-    const result = await usersCollection.deleteOne({ _id: ObjectId(userId) });
+        const result = await usersCollection.deleteOne({ _id: ObjectId(userId) });
 
-    if (result.deletedCount === 0) {
-      return res.status(400).send(PROFILE_DELETE_FAILED_ERROR);
+        if (result.deletedCount === 0) {
+            return res.status(400).send(PROFILE_DELETE_FAILED_ERROR);
+        }
+
+        res.status(200).send('Profile deleted successfully!');
+    } catch (error) {
+        next(error);
     }
-
-    res.status(200).send('Profile deleted successfully!');
-  } catch (error) {
-    next(error);
-  }
 });
 
 // Error handling middleware
 router.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
 
 module.exports = router;
