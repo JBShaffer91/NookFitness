005f1fc1ba596d488c0c98316d789c91484acc07
Justin Shaffer <justinbshaffer91@gmail.com>
2023-08-29 16:58:49 -0700

    Enhance server.js
    - Moved the Suggestic and ExerciseAPI3 key initializations closer to their usage for better organization
    - Enhanced error logging for better clarity during debugging
    - Removed unnecessary variable declaration

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,6 @@ const { Suggestic } = require('@suggestic/sdk');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const SUGGESTIC_API_KEY = process.env.SUGGESTIC_API_KEY;
-const suggesticClient = new Suggestic(SUGGESTIC_API_KEY);
-const EXERCISE_API3_KEY = process.env.EXERCISE_API3_KEY;
 
 // Middleware setup
 app.use(bodyParser.json());
@@ -25,7 +22,10 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/NookFitDB'
 const client = new MongoClient(MONGO_URI, { useUnifiedTopology: true });
 
 client.connect(err => {
-  if (err) throw err;
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  }
   console.log('Connected to MongoDB database.');
   app.locals.db = client.db('NookFitDB');
 });
@@ -40,40 +40,47 @@ app.get('/', (req, res) => {
   res.send('Nook Fitness API is running!');
 });
 
-// Suggestic API Route to get a meal plan
+// Suggestic API setup
+const SUGGESTIC_API_KEY = process.env.SUGGESTIC_API_KEY;
+const suggesticClient = new Suggestic(SUGGESTIC_API_KEY);
+
 app.get('/api/suggestic/mealplan', async (req, res) => {
   try {
-      const user = suggesticClient.getUser(process.env.SUGGESTIC_USER_ID);
-      const mealplan = await user.mealPlan();
-      res.json(mealplan);
+    const user = suggesticClient.getUser(process.env.SUGGESTIC_USER_ID);
+    const mealplan = await user.mealPlan();
+    res.json(mealplan);
   } catch (error) {
-      res.status(500).send('Error fetching data from Suggestic API.');
+    console.error('Error fetching data from Suggestic API:', error);
+    res.status(500).send('Error fetching data from Suggestic API.');
   }
 });
 
 // ExerciseAPI3 Route
+const EXERCISE_API3_KEY = process.env.EXERCISE_API3_KEY;
+
 app.get('/api/exercise/:name', async (req, res) => {
   const exerciseName = req.params.name;
   const options = {
-      method: 'GET',
-      url: `https://exerciseapi3.p.rapidapi.com/exercise/name/${exerciseName}`,
-      headers: {
-          'X-RapidAPI-Key': process.env.EXERCISE_API3_KEY,
-          'X-RapidAPI-Host': 'exerciseapi3.p.rapidapi.com'
-      }
+    method: 'GET',
+    url: `https://exerciseapi3.p.rapidapi.com/exercise/name/${exerciseName}`,
+    headers: {
+      'X-RapidAPI-Key': EXERCISE_API3_KEY,
+      'X-RapidAPI-Host': 'exerciseapi3.p.rapidapi.com'
+    }
   };
 
   try {
-      const response = await axios.request(options);
-      res.json(response.data);
+    const response = await axios.request(options);
+    res.json(response.data);
   } catch (error) {
-      res.status(500).send('Error fetching data from ExerciseAPI3.');
+    console.error('Error fetching data from ExerciseAPI3:', error);
+    res.status(500).send('Error fetching data from ExerciseAPI3.');
   }
 });
 
 // Error Handling
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  console.error('Server error:', err.stack);
   res.status(500).send('Something broke!');
 });
 
